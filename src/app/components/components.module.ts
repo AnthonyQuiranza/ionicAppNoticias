6129a86053a80e3607a3d67cc69af70adfdeecd9
anthonyquiranza <anthony.quiranza@upec.edu.ec>
2021-02-02 12:51:20 -0500

    as

--- src/app/components/components.module.ts
+++ src/app/components/components.module.ts
@@ -1,12 +1,21 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { IonicModule } from '@ionic/angular';
+import { NoticiasComponent } from './noticias/noticias.component';
+import { NoticiaComponent } from './noticia/noticia.component';
 
 
 
 @NgModule({
   declarations: [],
   imports: [
-    CommonModule
+    CommonModule,
+    IonicModule,
+    NoticiasComponent,
+    NoticiaComponent
+  ],
+  exports: [
+    NoticiasComponent
   ]
 })
 export class ComponentsModule { }
